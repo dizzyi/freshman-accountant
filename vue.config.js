afb4a55ffe7596779ea4a66eecaa3db0672fb679
dizzyi <deeralan827@gmail.com>
2021-02-17 16:50:36 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
-module.export = {
-    publicPath: '/freshman-accountant/'
-}
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/freshman-accountant/'
+      : '/'
+  }
